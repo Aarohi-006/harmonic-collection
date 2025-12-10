57fb7f1d2608e77f47ab47e35287a75c42342044
Aarohi-006 <lala934@newschool.edu>
2025-12-10 00:06:29 +0000

    changes

--- entry-10/script.js
+++ entry-10/script.js
@@ -1,3 +1,15 @@
-function openEntry(folder) {
-  window.open(`https://aarohi-006.github.io/harmonic-collection/${folder}/`, "_blank");
+function openEntry(num) {
+  const links = {
+    1: "https://aarohi-006.github.io/harmonic-collection/entry-1/",
+    2: "https://aarohi-006.github.io/harmonic-collection/entry-2/",
+    3: "https://aarohi-006.github.io/harmonic-collection/entry-03/",
+    4: "https://aarohi-006.github.io/harmonic-collection/entry--4/",
+    5: "https://aarohi-006.github.io/harmonic-collection/entry-5/",
+    6: "https://aarohi-006.github.io/harmonic-collection/entry-6/",
+    7: "https://aarohi-006.github.io/harmonic-collection/entry-7/",
+    8: "https://aarohi-006.github.io/harmonic-collection/entry-8/",
+    9: "https://aarohi-006.github.io/harmonic-collection/entry-9/"
+  };
+
+  window.open(links[num], "_blank");
 }
